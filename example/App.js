54c1e3d287a885dd8660ae14973c1cfe1170b754
Henning Hall <henning.hall@hotmail.com>
2018-04-19 20:21:36 +0200

    Renamed package

--- example/App.js
+++ example/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, StyleSheet} from 'react-native';
 
-import DatePicker from 'react-native-date-picker';
+import DatePicker from 'react-native-date-picker-x';
 
 export default class App extends Component {
 
