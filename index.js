858137df2384ee4bea6108871865ea69356dbc20
Henning Hall <henning.hall@hotmail.com>
2018-08-25 01:22:53 +0200

    Added textColor property (ios)

--- index.js
+++ index.js
@@ -1,58 +1,6 @@
-import {
-    Platform,
-    NativeModules,
-    NativeAppEventEmitter,
-    DatePickerIOS,
-    Text,
-    requireNativeComponent,
-    ViewPropTypes,
-    StyleSheet,
-} from 'react-native';
-import React, { Component } from 'react'
-import PropTypes from 'prop-types';
-import { DatePicker } from 'react-native-date-picker-x';
-
-const ios = Platform.OS === 'ios';
-
-const NativeDatePicker = requireNativeComponent(`DatePickerManager`, DatePickerAndroid, { nativeOnly: { onChange: true } });
-
-class DatePickerAndroid extends React.Component {
-    static propTypes = {
-        ...DatePickerIOS.propTypes,
-        fadeToColor: PropTypes.string,
-    }
-    static defaultProps = { 
-        mode: 'datetime',
-        minuteInterval: 1,
-        fadeToColor: '#ffffff',
-     };
-     
-    _onChange = e => this.props.onDateChange(new Date(parseInt(e.nativeEvent.date)));
-    _maximumDate = () => this.props.maximumDate && this.props.maximumDate.getTime();
-    _minimumDate = () => this.props.minimumDate && this.props.minimumDate.getTime();
-    render = () => (
-        <NativeDatePicker
-            {...this.props }
-            minimumDate={this._minimumDate()}
-            maximumDate={this._maximumDate()}
-            date={this.props.date.getTime()}            
-            onChange={this._onChange}
-            fadeToColor={this.props.fadeToColor}
-            style={[styles.picker, this.props.style]}
-        />
-    )
-}
-
-class DatePickerIOSWithSize extends React.Component {
-    render = () => <DatePickerIOS {...this.props} style={[styles.picker, this.props.style]} />
-}
+import { Platform } from 'react-native';
+import DatePickerIOS from './DatePickerIOS';
+import DatePickerAndroid from './DatePickerAndroid';
 
 
-const styles = StyleSheet.create({
-    picker: {
-        width: 310,
-        height: 180,
-    }
-})
-
-export default ios ? DatePickerIOSWithSize : DatePickerAndroid;
+export default Platform.OS === 'ios' ? DatePickerIOS : DatePickerAndroid
---
+++ ios/DatePickerX/DatePicker.m
@@ -0,0 +1,103 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import "DatePicker.h"
+
+#import "RCTUtils.h"
+#import "UIView+React.h"
+
+@interface DatePicker ()
+
+@property (nonatomic, copy) RCTBubblingEventBlock onChange;
+
+@end
+
+@implementation DatePicker
+
+
+#define UIColorFromRGB(rgbHex) [UIColor colorWithRed:((float)((rgbHex & 0xFF0000) >> 16))/255.0 green:((float)((rgbHex & 0xFF00) >> 8))/255.0 blue:((float)(rgbHex & 0xFF))/255.0 alpha:1.0]
+
+
+- (UIColor *) colorFromHexCode:(NSString *)hexString {
+    NSString *cleanString = [hexString stringByReplacingOccurrencesOfString:@"#" withString:@""];
+    if([cleanString length] == 3) {
+        cleanString = [NSString stringWithFormat:@"%@%@%@%@%@%@",
+                       [cleanString substringWithRange:NSMakeRange(0, 1)],[cleanString substringWithRange:NSMakeRange(0, 1)],
+                       [cleanString substringWithRange:NSMakeRange(1, 1)],[cleanString substringWithRange:NSMakeRange(1, 1)],
+                       [cleanString substringWithRange:NSMakeRange(2, 1)],[cleanString substringWithRange:NSMakeRange(2, 1)]];
+    }
+    if([cleanString length] == 6) {
+        cleanString = [cleanString stringByAppendingString:@"ff"];
+    }
+    
+    unsigned int baseValue;
+    [[NSScanner scannerWithString:cleanString] scanHexInt:&baseValue];
+    
+    float red = ((baseValue >> 24) & 0xFF)/255.0f;
+    float green = ((baseValue >> 16) & 0xFF)/255.0f;
+    float blue = ((baseValue >> 8) & 0xFF)/255.0f;
+    float alpha = ((baseValue >> 0) & 0xFF)/255.0f;
+    
+    return [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if ((self = [super initWithFrame:frame])) {
+        [self addTarget:self action:@selector(didChange)
+       forControlEvents:UIControlEventValueChanged];
+    }
+    
+    
+    unsigned result = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:@"#0000ff"];
+    
+    [scanner setScanLocation:1]; // bypass '#' character
+    [scanner scanHexInt:&result];
+    
+    
+    [self setValue:UIColorFromRGB(result) forKeyPath:@"textColor"];
+    SEL selector = NSSelectorFromString(@"setHighlightsToday:");
+    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[UIDatePicker instanceMethodSignatureForSelector:selector]];
+    BOOL no = NO;
+    [invocation setSelector:selector];
+    [invocation setArgument:&no atIndex:2];
+    [invocation invokeWithTarget:self];
+    
+    return self;
+}
+
+- (void)setTextColorProp:(NSString *)hexColor
+{
+    // Hex to int color
+    unsigned intColor = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:hexColor];
+    [scanner setScanLocation:1]; // bypass '#' character
+    [scanner scanHexInt:&intColor];
+    
+    // Setting picker text color
+    [self setValue:UIColorFromRGB(intColor) forKeyPath:@"textColor"];
+    SEL selector = NSSelectorFromString(@"setHighlightsToday:");
+    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[UIDatePicker instanceMethodSignatureForSelector:selector]];
+    BOOL no = NO;
+    [invocation setSelector:selector];
+    [invocation setArgument:&no atIndex:2];
+    [invocation invokeWithTarget:self];
+}
+
+
+RCT_NOT_IMPLEMENTED(- (instancetype)initWithCoder:(NSCoder *)aDecoder)
+
+- (void)didChange
+{
+    if (_onChange) {
+        _onChange(@{ @"timestamp": @(self.date.timeIntervalSince1970 * 1000.0) });
+    }
+}
+
+@end
+
