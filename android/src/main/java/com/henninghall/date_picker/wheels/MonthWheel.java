75c519a1706ebd60e723d6db13ae836c862a5534
Henning Hall <henning.hall@hotmail.com>
2018-09-10 21:58:40 +0200

    Bugfix: Month wheel has duplicate months.

--- android/src/main/java/com/henninghall/date_picker/wheels/MonthWheel.java
+++ android/src/main/java/com/henninghall/date_picker/wheels/MonthWheel.java
@@ -13,10 +13,10 @@ public class MonthWheel extends Wheel
     @Override
     void init() {
         final int min = 0;
-        final int max = 12;
+        final int max = 11;
         final Calendar cal = this.pickerView.getInitialDate();
         final SimpleDateFormat format = new SimpleDateFormat(this.getFormatTemplate(), this.pickerView.locale);
-        for (int i = min; i < max; ++i) {
+        for (int i = min; i <= max; ++i) {
             this.values.add(format.format(cal.getTime()));
             this.displayValues.add(format.format(cal.getTime()));
             cal.add(Calendar.MONTH, 1);
