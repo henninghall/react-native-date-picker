eba2c40b562042f0b8052fed39e918f7d55f27cc
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2019-04-26 18:52:23 -0300

    Added typings (#51)
    * Added typings
    
    Based on flow types
    
    * Update index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,80 @@
+import { Component } from 'react'
+import { ViewProps } from 'react-native'
+
+interface Props extends ViewProps {
+  textColor?: string
+  /**
+   * The currently selected date.
+   */
+  date?: Date
+
+  /**
+   * Provides an initial value that will change when the user starts selecting
+   * a date. It is useful for simple use-cases where you do not want to deal
+   * with listening to events and updating the date prop to keep the
+   * controlled state in sync. The controlled state has known bugs which
+   * causes it to go out of sync with native. The initialDate prop is intended
+   * to allow you to have native be source of truth.
+   */
+  initialDate?: Date
+
+  /**
+   * The date picker locale.
+   */
+  locale?: string
+
+  /**
+   * Maximum date.
+   *
+   * Restricts the range of possible date/time values.
+   */
+  maximumDate?: Date
+
+  /**
+   * Minimum date.
+   *
+   * Restricts the range of possible date/time values.
+   */
+  minimumDate?: Date
+
+  /**
+   * The interval at which minutes can be selected.
+   */
+  minuteInterval?: 1 | 2 | 3 | 4 | 5 | 6 | 10 | 12 | 15 | 20 | 30
+
+  /**
+   * The date picker mode.
+   */
+  mode?: 'date' | 'time' | 'datetime'
+
+  /**
+   * Date change handler.
+   *
+   * This is called when the user changes the date or time in the UI.
+   * The first and only argument is an Event. For getting the date the picker
+   * was changed to, use onDateChange instead.
+   */
+  onChange?: (event: object) => void
+
+  /**
+   * Date change handler.
+   *
+   * This is called when the user changes the date or time in the UI.
+   * The first and only argument is a Date object representing the new
+   * date and time.
+   */
+  onDateChange: (date: Date) => void
+
+  /**
+   * Timezone offset in minutes.
+   *
+   * By default, the date picker will use the device's timezone. With this
+   * parameter, it is possible to force a certain timezone offset. For
+   * instance, to show times in Pacific Standard Time, pass -7 * 60.
+   */
+  timeZoneOffsetInMinutes?: number
+}
+
+class DatePicker extends Component<Props> {}
+
+export default DatePicker
