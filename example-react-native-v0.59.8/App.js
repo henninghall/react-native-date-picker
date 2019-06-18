0f9d1d6c71f31e377c92469605a4a2630157efcd
Henning Hall <henning.hall@hotmail.com>
2019-06-18 20:37:07 +0100

    Added RN 0.59.8 project

---
+++ example-react-native-v0.59.8/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import DatePicker from "react-native-date-picker";
+
+export default class App extends Component {
+  state = { date: new Date() };
+
+  render = () => (
+    <View style={styles.container}>
+      <DatePicker
+        date={this.state.date}
+        onDateChange={date => this.setState({ date })}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFFFFF"
+  }
+});
