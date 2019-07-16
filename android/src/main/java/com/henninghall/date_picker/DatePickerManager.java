b9c90a310f85f900f09e7ca0c76a857bf3ef123c
Henning Hall <henning.hall@hotmail.com>
2019-07-16 13:20:18 +0200

    android X compatibility

--- android/src/main/java/com/henninghall/date_picker/DatePickerManager.java
+++ android/src/main/java/com/henninghall/date_picker/DatePickerManager.java
@@ -1,7 +1,7 @@
 package com.henninghall.date_picker;
 
 import android.content.res.Resources;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 import android.util.TypedValue;
 
