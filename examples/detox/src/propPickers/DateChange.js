4a0041cf21413dc68e2790fd21503d93ebb62ef1
Henning Hall <henning.hall@hotmail.com>
2020-07-28 13:08:19 +0200

    add timezone offset tests

--- examples/detox/src/propPickers/DateChange.js
+++ examples/detox/src/propPickers/DateChange.js
@@ -13,7 +13,6 @@ export default class extends Component {
             )
           }
         />
-        <Text />
         <Button
           title="Add 1 hour"
           onPress={() =>
@@ -22,12 +21,19 @@ export default class extends Component {
             )
           }
         />
-        <Text />
         <Button
           title="Add 24 hours"
           onPress={() =>
             this.props.onChange(
               new Date(this.props.value.getTime() + 60 * 60 * 24 * 1000)
+            )
+          }
+        />
+        <Button
+          title="Add 1 year"
+          onPress={() =>
+            this.props.onChange(
+              new Date(this.props.value.getTime() + 60 * 60 * 24 * 1000 * 365)
             )
           }
         />
