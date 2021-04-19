21fcab23375fbcee1e5890333bbbf97474b61e05
Jencir CJ <jencir.jamal@digia.com>
2021-04-19 15:34:56 +0300

    Added getLocaleStringResource function for getting localised string resource.

--- android/src/main/java/com/henninghall/date_picker/LocaleUtils.java
+++ android/src/main/java/com/henninghall/date_picker/LocaleUtils.java
@@ -61,4 +61,29 @@ public class LocaleUtils {
         return df instanceof SimpleDateFormat && ((SimpleDateFormat) df).toPattern().contains("a");
     }
 
+    public static String getLocaleStringResource(Locale requestedLocale, int resourceId, Context context) {
+        String result;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) { // use latest api
+            Configuration config = new Configuration(context.getResources().getConfiguration());
+            config.setLocale(requestedLocale);
+            result = context.createConfigurationContext(config).getText(resourceId).toString();
+        }
+        else { // support older android versions
+            Resources resources = context.getResources();
+            Configuration conf = resources.getConfiguration();
+            Locale savedLocale = conf.locale;
+            conf.locale = requestedLocale;
+            resources.updateConfiguration(conf, null);
+
+            // retrieve resources from desired locale
+            result = resources.getString(resourceId);
+
+            // restore original locale
+            conf.locale = savedLocale;
+            resources.updateConfiguration(conf, null);
+        }
+
+        return result;
+    }
+
 }
