c280c4270552bf64913091573c97c97278bdcc53
Oded-Zilkha-Blayberg <odedbla@gmail.com>
2022-02-21 21:31:17 +0200

    Fix: change wheels order when the locale language is hebrew/arabic

--- android/src/main/java/com/henninghall/date_picker/wheels/MonthWheel.java
+++ android/src/main/java/com/henninghall/date_picker/wheels/MonthWheel.java
@@ -44,7 +44,8 @@ public class MonthWheel extends Wheel
 
     @Override
     public Paint.Align getTextAlign() {
-        return Paint.Align.LEFT;
+        return LocaleUtils.isRtlLanguage(state.getLocaleLanguageTag())
+        ? Paint.Align.RIGHT : Paint.Align.LEFT;
     }
 
     @Override
