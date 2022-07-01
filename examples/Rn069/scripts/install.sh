e44d61b747761959f4c4a84e53d6962ff8034b7e
Henning Hall <henning.hall@hotmail.com>
2022-07-01 15:36:08 +0200

    no pips in file

--- examples/Rn069/scripts/install.sh
+++ examples/Rn069/scripts/install.sh
@@ -1,15 +1,15 @@
 
-yes | sdkmanager --licenses 
+sdkmanager --licenses 
 echo "✨ Accepted licences"
 
 # Install the emulator tool
-yes | sdkmanager --install emulator
+sdkmanager --install emulator
 echo "✨ Installed emulator"
 
 # Install an AOSP image that would later be used as the AVD's OS
-yes | sdkmanager --install "system-images;android-29;default;x86_64"
+sdkmanager --install "system-images;android-29;default;x86_64"
 echo "✨ Installed image"
 
 # Create an AVD with the image we've previously installed
-yes | avdmanager --verbose create avd --force --name Pixel_4_API_29 --abi x86_64 --device "pixel" --package "system-images;android-29;default;x86_64"
+avdmanager --verbose create avd --force --name Pixel_4_API_29 --abi x86_64 --device "pixel" --package "system-images;android-29;default;x86_64"
 echo "✨ Created AVD"
