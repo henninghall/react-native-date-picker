0ce036ec8b9c72dea08ad383f4bb45a90fd27358
Henning Hall <henning.hall@hotmail.com>
2022-07-01 15:35:17 +0200

    accept licences

--- examples/Rn069/scripts/install.sh
+++ examples/Rn069/scripts/install.sh
@@ -8,11 +8,6 @@ sdkmanager --install "system-images;android-29;default;x86_64"
 
 echo "✨ Installed image"
 
-# To do that automatically in a script, try this:
-sdkmanager --licenses 
-
-echo "✨ Accepted licences"
-
 # Create an AVD with the image we've previously installed
 avdmanager --verbose create avd --force --name Pixel_4_API_29 --abi x86_64 --device "pixel" --package "system-images;android-29;default;x86_64"
 
