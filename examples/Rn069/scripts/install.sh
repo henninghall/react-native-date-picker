8eaecef79220997c4bd04fbb2311d4a0fde137b7
Henning Hall <henning.hall@hotmail.com>
2022-07-01 14:20:20 +0200

    no yeses

--- examples/Rn069/scripts/install.sh
+++ examples/Rn069/scripts/install.sh
@@ -1,15 +1,15 @@
 # Install the emulator tool
-yes | sdkmanager --install emulator
+sdkmanager --install emulator
 
 echo "✨ Installed emulator"
 
 # Install an AOSP image that would later be used as the AVD's OS
-yes | sdkmanager --install "system-images;android-29;default;x86_64"
+sdkmanager --install "system-images;android-29;default;x86_64"
 
 echo "✨ Installed image"
 
 # To do that automatically in a script, try this:
-yes | sdkmanager --licenses 
+sdkmanager --licenses 
 
 echo "✨ Accepted licences"
 
