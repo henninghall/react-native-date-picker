02c0a546c37c2894f303f0906747a96d97758acb
Henning Hall <henning.hall@hotmail.com>
2022-07-11 12:59:23 +0200

    step 1

--- examples/Rn069/e2e/firstTest.e2e.js
+++ examples/Rn069/e2e/firstTest.e2e.js
@@ -8,16 +8,6 @@ describe('Example', () => {
   });
 
   it('should have welcome screen', async () => {
-    await expect(element(by.text('Welcome'))).toBeVisible();
+    await expect(element(by.text('Step One'))).toBeVisible();
   });
-
-  // it('should show hello screen after tap', async () => {
-  //   await element(by.id('hello_button')).tap();
-  //   await expect(element(by.text('Hello!!!'))).toBeVisible();
-  // });
-
-  // it('should show world screen after tap', async () => {
-  //   await element(by.id('world_button')).tap();
-  //   await expect(element(by.text('World!!!'))).toBeVisible();
-  // });
 });
