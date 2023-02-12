24ccb38f7bf701a979b828ccef51f40c22e53f0e
Henning Hall <henning.hall@hotmail.com>
2023-02-12 14:35:41 +0100

    fix rn version

---
+++ examples/detox/fix-rn-version.ts
@@ -0,0 +1,10 @@
+import fs from 'fs'
+
+const file = './android/build.gradle'
+const replace = /implementation 'com.facebook.react:react-native:\+'/g
+const replacement = "implementation 'com.facebook.react:react-native:0.59.10'"
+const encoding = 'utf8'
+
+const data = fs.readFileSync(file, encoding)
+const result = data.replace(replace, replacement)
+fs.writeFileSync(file, result, encoding)
