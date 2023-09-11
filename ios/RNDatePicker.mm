1cca1c9a7c4a2dd81e8b5c16c1522c24517ba083
Henning Hall <henning.hall@hotmail.com>
2023-09-11 15:56:57 +0200

    Revert "try fi import issue"
    This reverts commit 31c9ecff76d67612d7b8d5e0d06899cf96a03708.

--- ios/RNDatePicker.mm
+++ ios/RNDatePicker.mm
@@ -1,4 +1,7 @@
+#import "RNDatePicker.h"
+
 #ifdef RCT_NEW_ARCH_ENABLED
+#import "RCTConvert.h"
 #import <React/RCTConversions.h>
 #import <react/renderer/components/RNDatePickerSpecs/ComponentDescriptors.h>
 #import <react/renderer/components/RNDatePickerSpecs/EventEmitters.h>
@@ -14,8 +17,7 @@ using namespace facebook::react;
 #import "RCTComponent.h"
 #endif
 
-#import "RCTConvert.h"
-#import "RNDatePicker.h"
+
 #import "DatePicker.h"
 
 
