ca234c71c1a5daff47d7c2dbf25e329ce02fa55f
Henning Hall <henning.hall@hotmail.com>
2023-09-11 16:52:45 +0200

    import React/RCTConvert

--- ios/RNDatePickerManager.mm
+++ ios/RNDatePickerManager.mm
@@ -3,18 +3,7 @@
 #import <React/RCTViewManager.h>
 
 #import "RNDatePickerManager.h"
-
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-#import <React/RCTLog.h>
-
 #import "RCTConvert.h"
-
 #import "DatePicker.h"
 #import "RNDatePicker.h"
 
