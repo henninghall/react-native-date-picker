c2ea1bbe71cc41037329ae67ddcf90bacc2513b8
Henning Hall <henning.hall@hotmail.com>
2024-03-11 18:14:03 +0100

    cleanup

--- scripts/update-patch.sh
+++ scripts/update-patch.sh
@@ -2,6 +2,7 @@ DIR="android/src/main/java/com/henninghall/date_picker/generated"
 PATCH="scripts/NumberPicker.patch"
 
 curl "https://android.googlesource.com/platform/frameworks/base/+/main/core/java/android/widget/NumberPicker.java?format=text" | base64 -d > $DIR/NumberPickerOriginal.java
+
 cp $DIR/NumberPicker.java $DIR/NumberPickerChanged.java
 git diff --no-index $DIR/NumberPickerOriginal.java $DIR/NumberPickerChanged.java  > $PATCH
 rm $DIR/NumberPickerOriginal.java
