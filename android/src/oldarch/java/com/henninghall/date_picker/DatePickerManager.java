84d48a69cddb455d9d3a14bdb11ce1f35dc352e1
Henning Hall <henning.hall@hotmail.com>
2024-03-12 06:57:02 +0100

    remove variant prop

--- android/src/oldarch/java/com/henninghall/date_picker/DatePickerManager.java
+++ android/src/oldarch/java/com/henninghall/date_picker/DatePickerManager.java
@@ -16,7 +16,6 @@ import com.facebook.react.uimanager.annotations.ReactPropGroup;
 import com.henninghall.date_picker.DatePickerModule;
 import com.henninghall.date_picker.props.DividerHeightProp;
 import com.henninghall.date_picker.props.Is24hourSourceProp;
-import com.henninghall.date_picker.props.VariantProp;
 import com.henninghall.date_picker.props.DateProp;
 import com.henninghall.date_picker.props.LocaleProp;
 import com.henninghall.date_picker.props.MaximumDateProp;
@@ -43,7 +42,7 @@ public class DatePickerManager extends SimpleViewManager<PickerView>  {
 
     @ReactPropGroup(names = { DateProp.name, ModeProp.name, LocaleProp.name, MaximumDateProp.name,
             MinimumDateProp.name, TextColorProp.name, TimezoneOffsetInMinutesProp.name, MinuteIntervalProp.name,
-            VariantProp.name, DividerHeightProp.name, Is24hourSourceProp.name
+            DividerHeightProp.name, Is24hourSourceProp.name
     })
     public void setProps(PickerView view, int index, Dynamic value) {
         DatePickerManagerImpl.setProps(view, index, value, getClass());
