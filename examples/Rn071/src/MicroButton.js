3348812a0f825802711db8729ed69c7bff4fad88
Henning Hall <henning.hall@hotmail.com>
2024-04-19 14:18:26 +0200

    fix test

--- examples/Rn071/src/MicroButton.js
+++ examples/Rn071/src/MicroButton.js
@@ -3,7 +3,10 @@ import {Text, TouchableOpacity} from 'react-native';
 export const MicroButton = ({onPress, text}) => {
   return (
     <TouchableOpacity title={'MicroButton'} onPress={onPress}>
-      <Text style={{fontSize: 10}}>{text}</Text>
+      <MicroText text={text} style={{color: '#008'}} />
     </TouchableOpacity>
   );
 };
+export const MicroText = ({text, ...props}) => (
+  <Text style={[props.style, {fontSize: 10}]}>{text}</Text>
+);
