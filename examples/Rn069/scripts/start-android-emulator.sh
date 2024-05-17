1555455ceb672761170d7f4056f67b0ca33f2439
Henning Hall <henning.hall@hotmail.com>
2024-05-17 15:07:35 +0200

    chore: remove detox leftovers

--- examples/Rn069/scripts/start-android-emulator.sh
+++
@@ -1,4 +0,0 @@
-currentDir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-emulatorName=$(bash ${currentDir}/available-android-emulator.sh)
-echo "Starting $emulatorName ..."
-~/Library/Android/sdk/emulator/emulator -avd $emulatorName &
