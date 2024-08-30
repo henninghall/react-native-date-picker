e64d9ad22fefcff7bfff435506ce280646c0bbe8
Henning Hall <henning.hall@hotmail.com>
2024-08-30 22:31:03 +0200

    divider color fix

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const DatePickerWrapper = (props) => {
     <DatePicker
       {...props}
       textColor={colorToHex(getTextColor(props))}
-      dividerColor={colorToHex(getTextColor(props))}
+      dividerColor={colorToHex(getDividerColor(props))}
       theme={getTheme(props)}
       title={getTitle(props)}
       confirmText={props.confirmText ? props.confirmText : 'Confirm'}
@@ -41,6 +41,15 @@ const getTheme = (props) => {
   const scheme = Appearance.getColorScheme()
   if (scheme === null) return undefined
   return scheme
+}
+
+/** @param {Props} props **/
+const getDividerColor = (props) => {
+  if (props.dividerColor) return props.dividerColor
+  const theme = getTheme(props)
+  if (theme === 'dark') return 'white'
+  if (theme === 'light') return 'black'
+  return undefined
 }
 
 /** @param {Props} props **/
